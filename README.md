806787e10529ff1453a20022581bff52bd0b19c8
Edu Olivares <116183221+titodelas@users.noreply.github.com>
2023-04-19 15:03:02 +0200

    Just for fun

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# Reverse Brute Force
- Just try to guess the number the program is thinking lmao
+# Juego de adivinanza
+
+Este es un juego de adivinanza en Python en el que el jugador intenta adivinar un número generado aleatoriamente por el programa. El juego incluye diferentes niveles de dificultad y una opción personalizada donde el usuario puede definir el rango de números y el número máximo de intentos. Además, el juego también cuenta con un temporizador que limita el tiempo disponible para adivinar el número.
+
+## Cómo jugar
+
+1.  Clona este repositorio en tu ordenador.
+    
+2.  Abre un terminal en la carpeta donde clonaste el repositorio.
+    
+3.  Ejecuta el archivo `main.py` con Python.
+    
+4.  Selecciona una dificultad en la que quieres jugar. Las opciones son:
+    
+
+-   Fácil: 10 intentos, números del 1 al 50, 100 puntos y 30 segundos.
+-   Medio: 7 intentos, números del 1 al 100, 150 puntos y 60 segundos.
+-   Difícil: 5 intentos, números del 1 al 150, 200 puntos y 90 segundos.
+-   Personalizado: rango de números y número máximo de intentos personalizables, sin límite de tiempo.
+-   Infinito: sin límite de intentos ni de tiempo, números del 1 al 200.
+
+5.  Si seleccionas el nivel personalizado, deberás introducir el rango de números y el número máximo de intentos.
+    
+6.  Cuando comience el juego, tendrás que introducir un número para intentar adivinar el número generado por el programa. Si no adivinas el número, el programa te indicará si el número secreto es mayor o menor que el número que has introducido. Si adivinas el número, ganarás el juego.
+    
+
+## Características del juego
+
+-   El número máximo de intentos depende de la dificultad seleccionada, pero se pueden utilizar menos intentos para ganar.
+-   La puntuación varía según la dificultad y el número de intentos utilizados.
+-   El temporizador limita el tiempo disponible para adivinar el número, pero sólo se aplica en niveles de dificultad predefinidos.
+-   Si seleccionas la dificultad infinita o personalizada, no habrá límite de intentos ni de tiempo.
+-   El número de intentos restantes y el tiempo disponible se mostrarán en la pantalla.
+-   Los puntos y el tiempo restante se mostrarán en la pantalla.
+-   Puedes jugar en solitario o contra un amigo en el modo multijugador.
+
+## Cómo contribuir
+
+Si te gustaría contribuir a este proyecto, ¡eres más que bienvenido! Puedes hacerlo de varias maneras:
+
+-   Abriendo un _pull request_ para corregir errores, mejorar el código o añadir nuevas características.
+-   Informando de errores y problemas en el _issue tracker_.
+-   Aportando nuevas ideas y sugerencias en el _issue tracker_.
+
+## Créditos
+
+Este proyecto ha sido creado por titodelas y se distribuye bajo la licencia MIT. Puedes utilizar este código libremente para cualquier propósito, aunque te agradecería que mencionaras mi nombre como autor original.
---
+++ main.py
@@ -0,0 +1,96 @@
+import random
+import time
+import os
+
+
+def adivinanza(dificultad):
+    if dificultad == "facil":
+        numero = random.randint(1, 50)
+        intentos_max = 5
+        tiempo_max = 30
+    elif dificultad == "medio":
+        numero = random.randint(1, 100)
+        intentos_max = 7
+        tiempo_max = 60
+    elif dificultad == "dificil":
+        numero = random.randint(1, 150)
+        intentos_max = 10
+        tiempo_max = 90
+    elif dificultad == "personalizado":
+        print("Introduce el rango máximo del número a adivinar:")
+        maximo = int(input())
+        numero = random.randint(1, maximo)
+        print("Introduce el número máximo de intentos:")
+        intentos_max = int(input())
+        tiempo_max = 0
+    elif dificultad == "infinito":
+        numero = random.randint(1, 200)
+        intentos_max = 0
+        tiempo_max = 0
+    elif dificultad == "extremo":
+        numero = random.randint(1, 10)
+        intentos_max = 1
+        tiempo_max = 10
+    else:
+        print("Dificultad no válida.")
+        return
+
+    intentos = 0
+    puntos = 0
+    tiempo_inicio = time.time()
+
+    print("Adivina el número secreto del 1 al", maximo if dificultad == "personalizado" else 200 if dificultad not in [
+          "extremo"] else 10, "en un máximo de", intentos_max if intentos_max > 0 else "infinitos" if dificultad != "extremo" else "1", "intentos.")
+    while True:
+        intentos += 1
+        if intentos > intentos_max and intentos_max > 0:
+            print("Te has quedado sin intentos. El número secreto era", numero)
+            if dificultad == "extremo":
+                print("Se apagará el ordenador en 3 segundos.")
+                time.sleep(3)
+                os.system("shutdown /s /t 0")
+            break
+
+        if tiempo_max > 0 and (time.time() - tiempo_inicio) > tiempo_max:
+            print("Se ha agotado el tiempo. El número secreto era", numero)
+            break
+
+        intento = int(input("Intento #" + str(intentos) + ": "))
+
+        if intento == numero:
+            print("¡Acertaste el número en", intentos, "intentos!")
+            if intentos <= intentos_max and puntos > 0:
+                print("Ganaste", puntos, "puntos.")
+            break
+        elif intento < numero:
+            print("El número secreto es más grande.")
+        else:
+            print("El número secreto es más pequeño.")
+
+        if dificultad not in ["personalizado", "infinito", "extremo"]:
+            puntos -= 10
+            if intentos == intentos_max:
+                puntos -= 20
+            elif intentos == int(intentos_max/2):
+                puntos += 10
+            elif intentos == int(intentos_max/4):
+                puntos += 20
+
+            if puntos < 0:
+                puntos = 0
+                print("Tienes", puntos, "puntos")
+
+
+dificultad = input("""
+                                      Selecciona la dificultad 
+                ╔═══════════════════════════════════════════════════════════════════════╗
+                ║   facil            > 10 Intentos | 1 - 50  | 100 puntos | 30 segundos ║
+                ║   medio            > 7 Intentos  | 1 - 100 | 150 puntos | 60 segundos ║
+                ║   dificil          > 5 Intentos  | 1 - 150 | 200 puntos | 90 segundos ║
+                ║   infinito         > ∞ Intentos  | 1 - 200 |     --     | ∞ segundos  ║
+                ║   extremo          > 1 Intento   | 1 - 10  |     --     | 10 segundos ║
+                ║   personalizada    > Intentos, Rango y Tiempo personalizable          ║
+                ╚═══════════════════════════════════════════════════════════════════════╝
+                
+                                    Introduce tu dificultat >> """)
+adivinanza(dificultad)
